a26687a22fa527956abd5d22208779753a231df2
moshe malka <moshemalka2014@gmail.com>
2018-08-06 17:28:18 +0300

    1

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <header>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+    </header>
     <router-view/>
   </div>
 </template>
---
+++ client/src/components/Main.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      <app-header></app-header>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header'
+export default {
+  name: 'Main',
+  components:{
+      appHeader: Header
+  },
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
